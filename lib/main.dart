be7a8984387953500bb76b1c095924bd19d137d7
hammadpervez <hammadpervez6@gmail.com>
2021-08-06 14:41:25 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:notifications/ui/home/home.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Home(),
+    );
+  }
+}
