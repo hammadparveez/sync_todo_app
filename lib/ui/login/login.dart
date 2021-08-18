dcf157e52226cbe04fcdd8291295a7061accb9cf
hammadpervez <hammadpervez6@gmail.com>
2021-08-18 15:44:54 -0700

    logging

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -28,6 +28,7 @@ class _LoginState extends State<Login> {
     // TODO: implement initState
     super.initState();
     log("=============================LOGIN SCREEN==================================");
+    log("=============================LOGIN SCREEN==================================");
   }
 
   @override
